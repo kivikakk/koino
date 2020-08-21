21664fd85391a049f2f64bf9c72091a4265ce63d
Asherah Connor <ashe@kivikakk.ee>
2020-08-21 13:30:16 +1000

    wip

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # koino
 
-![Build Status](https://action-badges.now.sh/kivikakk/koino)
+![Build status](https://github.com/kivikakk/koino/workflows/Zig/badge.svg)
 ![Spec Status: 341/671](https://img.shields.io/badge/specs-341%2F671-red.svg)
 
 Zig port of [Comrak](https://github.com/kivikakk/comrak).
--- build.zig
+++ build.zig
@@ -1,39 +1,29 @@
 const std = @import("std");
-const Builder = std.build.Builder;
+const linkPcre = @import("vendor/libpcre.zig/build.zig").linkPcre;
 
-pub fn build(b: *Builder) void {
-    // Standard target options allows the person running `zig build` to choose
-    // what target to build for. Here we do not override the defaults, which
-    // means any target is allowed, and the default is native. Other options
-    // for restricting supported target set are available.
+pub fn build(b: *std.build.Builder) !void {
     const target = b.standardTargetOptions(.{});
-
-    // Standard release options allow the person running `zig build` to select
-    // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall.
     const mode = b.standardReleaseOptions();
 
     const exe = b.addExecutable("koino", "src/main.zig");
     exe.setTarget(target);
     exe.setBuildMode(mode);
-    addCommonRequirements(b, exe);
+    try addCommonRequirements(exe);
     exe.install();
 
     const run_cmd = exe.run();
     run_cmd.step.dependOn(b.getInstallStep());
-
     const run_step = b.step("run", "Run the app");
     run_step.dependOn(&run_cmd.step);
 
-    // I'm sure there's a way to deduplicate this with above.
     const test_exe = b.addTest("src/main.zig");
-    addCommonRequirements(b, test_exe);
-
+    try addCommonRequirements(test_exe);
     const test_step = b.step("test", "Run all the tests");
     test_step.dependOn(&test_exe.step);
 }
 
-fn addCommonRequirements(b: *Builder, o: *std.build.LibExeObjStep) void {
-    o.addPackagePath("libpcre", "vendor/libpcre.zig/src/main.zig");
-    o.addPackagePath("htmlentities", "vendor/htmlentities.zig/src/main.zig");
-    o.linkSystemLibrary("libpcre");
+fn addCommonRequirements(exe: *std.build.LibExeObjStep) !void {
+    exe.addPackagePath("libpcre", "vendor/libpcre.zig/src/main.zig");
+    exe.addPackagePath("htmlentities", "vendor/htmlentities.zig/src/main.zig");
+    try linkPcre(exe);
 }
