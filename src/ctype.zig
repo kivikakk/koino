ae8c9149240b6b7834ffca54e3f9482f064c4c7e
Asherah Connor <ashe@kivikakk.ee>
2020-08-25 10:37:24 +1000

    these are equivalent; corrected upstream
    See https://github.com/commonmark/cmark/pull/356.

--- src/ctype.zig
+++ src/ctype.zig
@@ -1,5 +1,5 @@
 const CMARK_CTYPE_CLASS: [256]u8 = [256]u8{
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 1, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0,
     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
     1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2,
     3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 2, 2, 2, 2, 2, 2,
@@ -39,4 +39,23 @@ pub fn isalnum(ch: u8) bool {
 
 pub fn isxdigit(ch: u8) bool {
     return (ch >= '0' and ch <= '9') or (ch >= 'a' and ch <= 'f') or (ch >= 'A' and ch <= 'F');
+}
+
+const std = @import("std");
+
+fn testEquivalence(name: []const u8, ours: fn (u8) bool, theirs: fn (u8) bool) void {
+    var i: u8 = 0;
+    while (true) : (i += 1) {
+        std.testing.expectEqual(ours(i), theirs(i));
+        if (i == 255) break;
+    }
+}
+
+test "cmark ctype vs std.ascii" {
+    testEquivalence("isspace", isspace, std.ascii.isSpace);
+    testEquivalence("ispunct", ispunct, std.ascii.isPunct);
+    testEquivalence("isdigit", isdigit, std.ascii.isDigit);
+    testEquivalence("isalpha", isalpha, std.ascii.isAlpha);
+    testEquivalence("isalnum", isalnum, std.ascii.isAlNum);
+    testEquivalence("isxdigit", isxdigit, std.ascii.isXDigit);
 }
