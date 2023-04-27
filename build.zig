6210d335c41cf77a6a57ab754ba7678d1a291f91
Asherah Connor <asherah@hrzn.ee>
2023-04-27 10:14:48 +1000

    update build, dependencies

--- build.zig
+++ build.zig
@@ -1,31 +1,44 @@
 const std = @import("std");
 const linkPcre = @import("vendor/libpcre/build.zig").linkPcre;
 
-pub fn build(b: *std.build.Builder) !void {
+pub fn build(b: *std.Build) !void {
     const target = b.standardTargetOptions(.{});
-    const mode = b.standardReleaseOptions();
+    const optimize = b.standardOptimizeOption(.{});
 
-    const exe = b.addExecutable("koino", "src/main.zig");
-    try addCommonRequirements(exe, target, mode);
-    exe.install();
+    var deps = std.StringHashMap(*std.build.Module).init(b.allocator);
+    try deps.put("libpcre", b.addModule("libpcre", .{ .source_file = .{ .path = "vendor/libpcre/src/main.zig" } }));
+    try deps.put("htmlentities", b.addModule("htmlentities", .{ .source_file = .{ .path = "vendor/htmlentities/src/main.zig" } }));
+    try deps.put("clap", b.addModule("clap", .{ .source_file = .{ .path = "vendor/zig-clap/clap.zig" } }));
+    try deps.put("zunicode", b.addModule("zunicode", .{ .source_file = .{ .path = "vendor/zunicode/src/zunicode.zig" } }));
 
-    const run_cmd = exe.run();
+    const exe = b.addExecutable(.{
+        .name = "koino",
+        .root_source_file = .{ .path = "src/main.zig" },
+        .target = target,
+        .optimize = optimize,
+    });
+    try addCommonRequirements(exe, &deps);
+    b.installArtifact(exe);
+
+    const run_cmd = b.addRunArtifact(exe);
     run_cmd.step.dependOn(b.getInstallStep());
     const run_step = b.step("run", "Run the app");
     run_step.dependOn(&run_cmd.step);
 
-    const test_exe = b.addTest("src/main.zig");
-    try addCommonRequirements(test_exe, target, mode);
+    if (b.args) |args| {
+        run_cmd.addArgs(args);
+    }
+
+    const test_exe = b.addTest(.{ .name = "test", .root_source_file = .{ .path = "src/main.zig" } });
+    try addCommonRequirements(test_exe, &deps);
     const test_step = b.step("test", "Run all the tests");
     test_step.dependOn(&test_exe.step);
 }
 
-fn addCommonRequirements(exe: *std.build.LibExeObjStep, target: std.zig.CrossTarget, mode: std.builtin.Mode) !void {
-    exe.setTarget(target);
-    exe.setBuildMode(mode);
-    exe.addPackagePath("libpcre", "vendor/libpcre/src/main.zig");
-    exe.addPackagePath("htmlentities", "vendor/htmlentities/src/main.zig");
-    exe.addPackagePath("clap", "vendor/zig-clap/clap.zig");
-    exe.addPackagePath("zunicode", "vendor/zunicode/src/zunicode.zig");
-    try linkPcre(exe);
+fn addCommonRequirements(cs: *std.build.CompileStep, deps: *const std.StringHashMap(*std.build.Module)) !void {
+    var it = deps.iterator();
+    while (it.next()) |entry| {
+        cs.addModule(entry.key_ptr.*, entry.value_ptr.*);
+    }
+    try linkPcre(cs);
 }
